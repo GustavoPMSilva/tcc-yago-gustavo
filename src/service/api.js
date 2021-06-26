566e177650883345cade2b76ffbe422efc3c24f5
gustavopmsilva <gustavopmirandasilva@gmail.com>
2021-06-26 19:21:40 -0300

    Changed backend to heroku back

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const api = Axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+  baseURL: "https://geprofi-back.herokuapp.com/api/v1/",
   headers: {
     Accept: "application/json",
     Authorization: "Bearer " + localStorage.getItem("@GeProFi:token"),
