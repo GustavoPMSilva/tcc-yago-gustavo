b122cdf423b3c3873d341e45125e46ec59da72b2
gustavopmsilva <gustavopmirandasilva@gmail.com>
2021-06-27 15:56:21 -0300

    Getting base url from env var

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,7 @@
 import Axios from "axios";
 
 export const api = Axios.create({
-  //baseURL: "http://localhost:8080/api/v1/",
-  baseURL: "https://geprofi-back.herokuapp.com/api/v1/",
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     Accept: "application/json",
     Authorization: "Bearer " + localStorage.getItem("@GeProFi:token"),
