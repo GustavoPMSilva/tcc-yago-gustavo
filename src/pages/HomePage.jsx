290c6619734169fe6a96a2e82315686bd9b9e6ad
gustavopmsilva <gustavopmirandasilva@gmail.com>
2021-10-15 21:10:16 -0300

    Changed favicon and added logo to home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { useApi } from "../contexts/ApiContext";
 import SignedInHomePage from "./SignedInHomePage";
+import logo from "../logo.jpg";
 
 function HomePage() {
   const { signed } = useApi();
 
-  return <>{signed ? <SignedInHomePage /> : <p>Home</p>}</>;
+  return (
+    <>
+      {signed ? (
+        <SignedInHomePage />
+      ) : (
+        <>
+          <br />
+          <img src={logo} class="center" alt="Logo" />
+        </>
+      )}
+    </>
+  );
 }
 
 export default HomePage;
