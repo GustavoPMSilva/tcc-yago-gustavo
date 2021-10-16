d57777000b2f3665a3edbd92552d3e48d7cf0514
gustavopmsilva <gustavopmirandasilva@gmail.com>
2021-10-15 23:41:31 -0300

    added button to delete field of interest

--- src/pages/FieldOfInterestPage.jsx
+++ src/pages/FieldOfInterestPage.jsx
@@ -5,15 +5,18 @@ import { Container, Typography, Button } from "@material-ui/core";
 import UserList from "../components/UserList";
 import { useApi } from "../contexts/ApiContext";
 import { GpfTextField } from "../components/core";
+import { AlertDialog } from "../components/dialog";
 
 function FieldOfInterestPage() {
   let history = useHistory();
-  const { apiGet, apiPut } = useApi();
+  const { apiGet, apiPut, apiDelete } = useApi();
   const { id } = useParams();
   const [fieldOfInterest, setFieldOfInterest] = useState({});
   const [fieldOfInterestUserList, setFieldOfInterestUserList] = useState([]);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [openDeleteFieldOfInterest, setOpenDeleteFieldOfInterest] =
+    useState(false);
 
   useEffect(() => {
     function onFieldOfInterestLoaded(data) {
@@ -33,6 +36,44 @@ function FieldOfInterestPage() {
     apiGet(`field/${id}`, onFieldOfInterestLoaded, onError);
     apiGet(`field/${id}/users`, onListLoaded, onError);
   }, []);
+
+  const handleClickOpenDeleteFieldOfInterest = () => {
+    setOpenDeleteFieldOfInterest(true);
+  };
+
+  const handleCloseDeleteFieldOfInterest = (del) => {
+    setOpenDeleteFieldOfInterest(false);
+    if (del) deleteFieldOfInterest();
+  };
+
+  function deleteFieldOfInterest() {
+    apiDelete(`field/${fieldOfInterest.id}`, () => {
+      history.push("/");
+    });
+  }
+
+  function showButton() {
+    if (fieldOfInterestUserList.length === 0) {
+      return (
+        <>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#eb2915" }}
+            onClick={handleClickOpenDeleteFieldOfInterest}
+            fullWidth
+          >
+            Excluir área de interesse
+          </Button>
+          <AlertDialog
+            open={openDeleteFieldOfInterest}
+            handleClose={handleCloseDeleteFieldOfInterest}
+            title="Excluir área de interesse"
+            body={`Tem certeza que deseja excluir ${fieldOfInterest.name}? Essa ação não poderá ser desfeita.`}
+          />
+        </>
+      );
+    }
+  }
 
   function saveChanges() {
     fieldOfInterest.name = name;
@@ -79,6 +120,7 @@ function FieldOfInterestPage() {
       <br />
       <UserList userList={fieldOfInterestUserList} />
       <br />
+      {showButton()}
     </Container>
   );
 }
